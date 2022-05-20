1e5019fbcfff6fb0cf77e1276f64b451cfa329a7
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2022-05-19 23:01:32 -0300

    adição das categorias dos produtos

--- sql/dml.sql
+++ sql/dml.sql
@@ -115,58 +115,73 @@ VALUES
     ()
 ;
 
+INSERT INTO categoria_produto (
+    ds_categoria
+    , dt_ultima_alteracao
+    , dt_criacao
+)
+VALUES
+    ('VITAMINAS')
+    , ('SUCOS')
+    , ('BEBIDAS')
+    , ('MISTOS')
+    , ('PORÇÕES')
+    , ('LANCHES GOURMET')
+;
+
 INSERT INTO produto (
-    no_produto
+    cd_categoria
+    , no_produto
     , vl_unitario
     , qt_estoque
 )
 VALUES
     -- VITAMINAS
-    ('VITAMINA DE ABACAXI', 7.00, 10)
-    , ('VITAMINA DE MORANGO', 7.00, 10)
-    , ('VITAMINA DE ABACAXI COM HORTELÃ', 7.00, 10)
-    , ('VITAMINA DE GRAVIOLA', 7.00, 10)
-    , ('VITAMINA DE CAJU', 7.00, 10)
-    , ('VITAMINA DE MARACUJÁ', 7.00, 10)
-    , ('VITAMINA DE GOIABA', 7.00, 0)
-    , ('VITAMINA DE MANGA', 7.00, 10)
-    , ('VITAMINA DE ACEROLA', 7.00, 10)
+    (1, 'VITAMINA DE ABACAXI', 7.00, 10)
+    , (1, 'VITAMINA DE MORANGO', 7.00, 10)
+    , (1, 'VITAMINA DE ABACAXI COM HORTELÃ', 7.00, 10)
+    , (1, 'VITAMINA DE GRAVIOLA', 7.00, 10)
+    , (1, 'VITAMINA DE CAJU', 7.00, 10)
+    , (1, 'VITAMINA DE MARACUJÁ', 7.00, 10)
+    , (1, 'VITAMINA DE GOIABA', 7.00, 0)
+    , (1, 'VITAMINA DE MANGA', 7.00, 10)
+    , (1, 'VITAMINA DE ACEROLA', 7.00, 10)
     -- SUCOS
-    , ('SUCO DE ABACAXI', 6.00, 10)
-    , ('SUCO DE MORANGO', 6.00, 10)
-    , ('SUCO DE ABACAXI COM HORTELÃ', 6.00, 10)
-    , ('SUCO DE GRAVIOLA', 6.00, 10)
-    , ('SUCO DE CAJU', 6.00, 10)
-    , ('SUCO DE MARACUJÁ', 6.00, 10)
-    , ('SUCO DE GOIABA', 6.00, 10)
-    , ('SUCO DE MANGA', 6.00, 10)
-    , ('SUCO DE ACEROLA', 6.00, 10)
+    , (2, 'SUCO DE ABACAXI', 6.00, 10)
+    , (2, 'SUCO DE MORANGO', 6.00, 10)
+    , (2, 'SUCO DE ABACAXI COM HORTELÃ', 6.00, 10)
+    , (2, 'SUCO DE GRAVIOLA', 6.00, 10)
+    , (2, 'SUCO DE CAJU', 6.00, 10)
+    , (2, 'SUCO DE MARACUJÁ', 6.00, 10)
+    , (2, 'SUCO DE GOIABA', 6.00, 10)
+    , (2, 'SUCO DE MANGA', 6.00, 10)
+    , (2, 'SUCO DE ACEROLA', 6.00, 10)
     -- BEBIDAS
-    , ('ÁGUA MINERAL', 3.00, 10)
-    , ('ÁGUA MINERAL COM GÁS', 3.50, 10)
-    , ('ÁGUA TÔNICA', 6.00, 10)
-    , ('REFRIGERANTE LATA', 6.00, 10)
-    , ('REFRIGERANTE 1,5L', 10.00, 10)
-    , ('REFRIGERANTE 2L', 11.00, 10)
-    , ('SWEEPS CITRUS', 6.00, 10)
+    , (3, 'ÁGUA MINERAL', 3.00, 10)
+    , (3, 'ÁGUA MINERAL COM GÁS', 3.50, 10)
+    , (3, 'ÁGUA TÔNICA', 6.00, 10)
+    , (3, 'REFRIGERANTE LATA', 6.00, 10)
+    , (3, 'REFRIGERANTE 1,5L', 10.00, 10)
+    , (3, 'REFRIGERANTE 2L', 11.00, 10)
+    , (3, 'SWEEPS CITRUS', 6.00, 10)
     -- MISTOS
-    , ('MISTO', 9.00, 10)
-    , ('MISTO EGG', 12.00, 10)
-    , ('MISTÃO', 12.00, 10)
-    , ('MISTO LIGHT', 12.00, 10)
+    , (4, 'MISTO', 9.00, 10)
+    , (4, 'MISTO EGG', 12.00, 10)
+    , (4, 'MISTÃO', 12.00, 10)
+    , (4, 'MISTO LIGHT', 12.00, 10)
     -- PORÇÕES
-    , ('PORÇÃO DE FRITAS', 19.50, 10)
-    , ('PORÇÃO DE FRITAS COM CHEDDAR E BACON', 14.50, 10)
-    , ('PORÇÃO DE FRITAS COM COSTELA', 16.50, 10)
-    , ('PORÇÃO DE ANEL DE CEBOLA', 18.00, 10)
-    , ('PORÇÃO DE MINI CHICKEN', 16.00, 10)
-    , ('PORÇÃO DE MINI COXINHAS', 16.00, 10)
+    , (5, 'PORÇÃO DE FRITAS', 19.50, 10)
+    , (5, 'PORÇÃO DE FRITAS COM CHEDDAR E BACON', 14.50, 10)
+    , (5, 'PORÇÃO DE FRITAS COM COSTELA', 16.50, 10)
+    , (5, 'PORÇÃO DE ANEL DE CEBOLA', 18.00, 10)
+    , (5, 'PORÇÃO DE MINI CHICKEN', 16.00, 10)
+    , (5, 'PORÇÃO DE MINI COXINHAS', 16.00, 10)
     -- LANCHES GOURMET
-    , ('FIRE BURGER', 20.00, 10)
-    , ('MAGNÍFICO', 28.00, 10)
-    , ('CALABRESA CRISPY', 25.00, 10)
-    , ('VIP BURGUER', 25.00, 10)
-    , ('PIG BURGUER', 20.00, 10)
+    , (6, 'FIRE BURGER', 20.00, 10)
+    , (6, 'MAGNÍFICO', 28.00, 10)
+    , (6, 'CALABRESA CRISPY', 25.00, 10)
+    , (6, 'VIP BURGUER', 25.00, 10)
+    , (6, 'PIG BURGUER', 20.00, 10)
 ;
 
 INSERT INTO item_pedido (
